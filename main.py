9ef40f5e7c8e5bb618a447e92f4c8f03574b55e5
marin <clement.marin@laplateforme.io>
2023-01-18 12:28:37 +0100

    [ADD] Basic tkinter UI

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import tkinter as tk
 import hashlib
 import json
 
+password_show = False
 
 def check_password(password):
     has_uppercase = False
@@ -22,52 +24,88 @@ def check_password(password):
     return False
 
 
-while True:
-    print("If you want to add a password press 1:")
-    print("If you want to show current saved password press 2:")
-    print("If you want to exit program press 3:")
-    choice = input("Choose: ")
-
-    if choice == '1':
-        password = input("Choose a Password : ")
-        if check_password(password):
-            print("Password Allowed")
-            break
-        else:
-            print("Invalid password. It must contain at least 8 characters, one lowercase letter, one uppercase letter, one number and one special character (~,`,!,@,#,$,%,^,&,*,(,),-,_,+,=,{,},[,],|,:,;,<,>,,,.,?,")
-    elif choice == '2':
-        print("Passwords : ")
-        with open("passwords.json", "r") as file:
-            data = json.load(file)
-            print(data)
-        exit()
-    elif choice == '3':
-        exit()
+def on_add():
+    password = password_entry.get()
+    if check_password(password):
+        print("Password Allowed")
     else:
-        print("IMPUT ERROR")
+        print("Invalid password. It must contain at least 8 characters, one lowercase letter, one uppercase letter, one number and one special character (!, @, #, $, %, ^, &, *).")
+        return
+    password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+    try:
+        with open("passwords.json", "r") as file:
+            passwords = json.load(file)
+    except:
+        passwords = {}
+
+    if password_hash in passwords:
+        print("Password already saved")
+    else:
+        passwords[password_hash] = password
+
+    with open("passwords.json", "w") as file:
+        json.dump(passwords, file)
+
+    print("Passwords : ")
+    for key, value in passwords.items():
+        print(key + " : " + value)
 
 
-password_hash = hashlib.sha256(password.encode()).hexdigest()
+def on_clear():
+    password_list.delete(0, tk.END)
 
-try:
+
+def on_show():
     with open("passwords.json", "r") as file:
-        passwords = json.load(file)
-except:
-    passwords = {}
+        data = json.load(file)
+        for key, value in data.items():
+            password_list.insert(tk.END, key + " : " + value)
 
-if password_hash in passwords:
-    print("Password already saved")
-else:
-    passwords[password_hash] = password
 
-with open("passwords.json", "w") as file:
-    json.dump(passwords, file)
+def show_clear():
+    global password_show
+    if password_show == False:
+        password_entry.configure(show="")
+        password_show = True
+    else:
+        password_entry.configure(show="*")
+        password_show = False
 
-print("Passwords : ")
-for key, value in passwords.items():
-    print(key + " : " + value)
 
-"""
-Ici dans le json le mot de passe hashé est associé à son mot de pas en clair, ce qui n'a pas vraiment de sens d'un point de
-vue sécuriter, mais j'ai fait cela pour une clarté de correction.
-"""
+def on_exit():
+    exit()
+
+
+root = tk.Tk()
+root.title("Password Manager")
+root.geometry("650x300")
+
+password_label = tk.Label(root, text="Password:")
+password_label.grid(row=0, column=0)
+
+password_entry = tk.Entry(root, show="*")
+password_entry.grid(row=0, column=1, pady=20)
+
+show_current_password = tk.Button(root, text="Show", command=show_clear)
+show_current_password.grid(row=0, column=2)
+
+add_button = tk.Button(root, text="Add Password", command=on_add)
+add_button.grid(row=0, column=3)
+
+show_button = tk.Button(root, text="Show Passwords", command=on_show)
+show_button.grid(row=2, column=0)
+
+clear_button = tk.Button(root, text="Clear Passwords", command=on_clear)
+clear_button.grid(row=2, column=1)
+
+exit_button = tk.Button(root, text="Exit", command=on_exit)
+exit_button.grid(row=2, column=4)
+
+password_list = tk.Listbox(root, width=100)
+password_list.grid(row=1, column=0, columnspan=10)
+
+root.mainloop()
+
+
+root.mainloop()
